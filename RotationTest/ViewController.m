c9dbe84b69d60b76b9205a594f1a944abe06c823
student <none@none.com>
2014-11-10 10:57:18 -0800

    removing useless comments

--- RotationTest/ViewController.m
+++ RotationTest/ViewController.m
@@ -49,21 +49,6 @@ const int TOP_MARGIN = 50;
     
     int size = screenBounds.size.width;
 
-    
-    // As gridView animates, the coordinates of its grame rotates with it. So, for example, if it
-    // rotates by 90 degrees, then its origin is no longer at (0, 0) with respect to its superview,
-    // it is at (widthOfSubsquare, 0). So, after the rotation, the coordinates of the points that
-    // you tap in the window, when located using:
-    
-    //     CGPoint p = [tapObject locationInView:self.gridView];
-
-    // will be reported with respect to the new origin of gridView, which is no longer consistent
-    // with the grid as it appears to the user. So, the tap  points have to get converted back to
-    // the user's view point. To do that, I have created a backView. backView will contain the
-    // gridView. After the gridView rotates, we get the location of taps in gridView and then
-    // we ask for the location of the point to be converted to the coordiantes of the backView. Since
-    // the backView doesn't rotate, its origin is consistent with what the user expects to see.
-
     self.backView = [[UIView alloc] initWithFrame:_gridFrame];
     self.gridView = [[UIView alloc] initWithFrame: CGRectMake(0, 0, size, size)];
     [self.backView addSubview:self.gridView];
@@ -102,9 +87,6 @@ const int TOP_MARGIN = 50;
 
 -(void) didTapTheView: (UITapGestureRecognizer *) tapObject
 {
-    // p is the location of the tap in self.gridView. We convert it to the coordinates of the
-    // self.backView, which is unchanged.
-    
     gameStarted = YES;
     CGPoint bp = [tapObject locationInView:self.backView];
 
